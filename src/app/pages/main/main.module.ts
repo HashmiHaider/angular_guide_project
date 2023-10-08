3090dd934bc4e95f5e4523eb4b6bce899e8d57a5
AnumMujahid <amujahid.bese18seecs@student.nust.edu.pk>
2023-10-08 22:48:46 +0500

    Workking on main dashboard

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MaterialModule } from 'src/app/material_module/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MainComponent } from './main.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 const login_routes: Routes = [
   {
@@ -13,7 +14,7 @@ const login_routes: Routes = [
   },
 ];
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, TopBarComponent],
   imports: [
     CommonModule,
     MaterialModule,
