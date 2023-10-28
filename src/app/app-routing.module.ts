69b7a17ed78dc99ec3d48df9f6046ae4bbe9418d
ahmadhassanch <ahmadhassanch@hotmail.com>
2023-10-28 17:22:36 +0500

    our general table working with hardcoded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,21 @@ const routes: Routes = [
     path: '',
     loadChildren: () =>
       import('./pages/auth/auth.module').then((p) => p.AuthModule),
+    // import('./pages/main/main.module').then((p) => p.MainModule),
+  },
+  {
+    path: 'table',
+    loadChildren: () =>
+      // import('./pages/auth/auth.module').then((p) => p.AuthModule),
+      import('./pages/main/main.module').then((p) => p.MainModule),
+  },
+  {
+    path: 'table-direct',
+    loadChildren: () =>
+      // import('./pages/auth/auth.module').then((p) => p.AuthModule),
+      import('./shared/cc-general-table/cc-general-table.module').then(
+        (p) => p.CCGeneralTableModule
+      ),
   },
 ];
 
